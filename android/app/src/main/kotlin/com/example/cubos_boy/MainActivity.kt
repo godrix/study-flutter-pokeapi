52e75b216aad2cb704d22c557f0bc7c8b34ea0c1
Carlos Godri <cggodri@gmail.com>
2021-04-17 12:20:42 -0300

    started project cubos boy

---
+++ android/app/src/main/kotlin/com/example/cubos_boy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cubos_boy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/poke_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PokeView extends StatefulWidget {
+  @override
+  _PokeViewState createState() => _PokeViewState();
+}
+
+class _PokeViewState extends State<PokeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
